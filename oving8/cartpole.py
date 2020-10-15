4ab9f22edf9928b3acf69c05af7d1d663953e26c
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-10-15 17:09:01 +0200

    first task done

---
+++ oving8/cartpole.py
@@ -0,0 +1,105 @@
+from sklearn.preprocessing import KBinsDiscretizer
+import numpy as np
+from typing import Tuple
+import gym, time, math
+
+# VARIABLES
+
+min_learning_rate = 0.1
+min_exploration_rate = 0.1
+training_episodes = 500
+run_episodes = 3
+discount = 1
+learning_rate = 0
+exploration_rate = 0
+decay = 25
+
+
+# FUNCTIONS
+
+
+# Continuous to Discrete. Q learning demands discrete states, but this problem has continuous states.
+def discretizer(_, __, angle, pole_velocity) -> Tuple[int, ...]:
+    """Convert continues state intro a discrete state"""
+    est = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='uniform')
+    est.fit([lower_bounds, upper_bounds])
+    return tuple(map(int, est.transform([[angle, pole_velocity]])[0]))
+
+
+# Chooses if a known action or a random action will be taken.
+def select_action(state_value, er):
+    if np.random.random() < er:
+        return env.action_space.sample()
+    else:
+        return np.argmax(Q_table[state_value])
+
+
+# Decays the learning rate so that the model learns more at the beginning.
+def decay_rate(n, min_value):
+    return max(min_value, min(1.0, 1.0 - math.log10((n + 1) / decay)))
+
+
+# EXECUTABLE
+
+
+# Get cartpole environment
+env = gym.make("CartPole-v0")
+env.reset()
+
+# Define bins
+bins = (6, 12)
+lower_bounds = [env.observation_space.low[2], -math.radians(50)]
+upper_bounds = [env.observation_space.high[2], math.radians(50)]
+
+# Initial Q-table
+Q_table = np.zeros(bins + (env.action_space.n,))
+
+# Training
+for episode in range(training_episodes):
+    current_state = discretizer(*env.reset())
+    done = False
+    while not done:
+        # Decay. Uses episode number as variable.
+        learning_rate = decay_rate(episode, min_learning_rate)
+        exploration_rate = decay_rate(episode, min_exploration_rate)
+
+        # Decide known action or random action.
+        action = select_action(current_state, exploration_rate)
+
+        # Increment environment
+        properties, reward, done, _ = env.step(action)
+        new_state = discretizer(*properties)
+
+        # Update Q-Table
+        future_optimal_value = np.max(Q_table[new_state])
+        learned_value = reward + discount * future_optimal_value
+        old_value = Q_table[current_state][action]
+        Q_table[current_state][action] = (1 - learning_rate) * old_value + learning_rate * learned_value
+
+        current_state = new_state
+
+        # env.render()
+
+print("Training Complete")
+input("Ready to start...")
+
+# Run
+for episode in range(run_episodes):
+    current_state = discretizer(*env.reset())
+    done = False
+    start = time.time()
+    while not done:
+        # Decide known action or random action.
+        action = select_action(current_state, exploration_rate)
+
+        # Increment environment
+        properties, reward, done, _ = env.step(action)
+        new_state = discretizer(*properties)
+        current_state = new_state
+
+        # Render the cartpole environment
+        env.render()
+
+    # Print time
+    end = time.time()
+    print("Time %s" % (end - start))
---
+++ oving8/readme.md
@@ -0,0 +1,10 @@
+# Oppgave 8
+
+## 1 Q-Læring: Cartpole
+Implementer Q-læring og bruk det for å løse cartpole-environmentet.
+
+https://gym.openai.com/envs/CartPole-v0/
+
+https://github.com/openai/gym
+
+## 2
